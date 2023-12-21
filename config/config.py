031457681b3d398284a3814c064084e5d28cf328
郑子涛 <2749060738@qq.com>
2023-12-21 23:33:45 +0800

    更新了一下开发日志

---
+++ config/config.py
@@ -0,0 +1,34 @@
+# 允许使用数字
+USE_NUMBER=False
+
+# 允许使用小写字母
+USE_LOWER_ALPHABET=True
+
+# 允许使用大写字母
+USE_UPPER_ALPHABET=False
+
+# 允许使用全部ASCII字符
+USE_ALL_ASCII=False
+
+LOWER_ALPHABET_LIST=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+UPPER_APLPHABET_LIST=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+NUMBER_LIST=['0','1','2','3','4','5','6','7','8','9']
+
+ASCII_LIST=[chr(x) for x in range(1,128)]
+
+CANDIDATES=[]
+
+if(USE_ALL_ASCII):
+    CANDIDATES += USE_ALL_ASCII
+else:
+    if(USE_NUMBER):
+        CANDIDATES += NUMBER_LIST
+    if(USE_UPPER_ALPHABET):
+        CANDIDATES += UPPER_APLPHABET_LIST
+    if(USE_LOWER_ALPHABET):
+        CANDIDATES += LOWER_ALPHABET_LIST
+
+
+print("CANDIDATES: ", CANDIDATES)
--- test/testScheduler.py
+++ test/testScheduler.py
@@ -3,53 +3,8 @@ sys.path.append(".")
 
 from src.scheduler import *
 
-
-def test_char_flip():
-    print(char_flip("0123456789", 1, 10, 3))   
-
-def test_char_del():
-    print(char_del("0123456789", 3, 2))
-
-def test_char_ins():
-    print(char_ins("123456789", 2, 7))
-
-def test_havoc():
-    print(Havoc("123456789"))
-
-def test_splice():
-    print(1, Splice("12345789", "abcdefg"))
-    print(2, Splice("12345", "abc"))
-    print(3, Splice("123", "abcd"))
-    print(4, Splice("1", "a"))
-    print(5, Splice("1", ""))
-    print(6, Splice("", "1"))
-    print(7, Splice("", ""))
-
-def test_char_change():
-    print(char_change("123456789", 1))
-    print(char_change("123456789", 2))
-
-def test_bit_revert():
-    print(bit_revert("12345", 5))
-
-def test_case_conversion():
-    print(case_conversion("aBcde", 0))
-    print(case_conversion("aBcde", 1))
-    print(case_conversion("aBcde", 2))
-
-def test_repeat_pattern():
-    print(repeat_pattern("123456789", 1, 9))
-    print(repeat_pattern("123456789", 2, 9))
-    print(repeat_pattern("123456789", 2, 8))
-
-def test_boundary_change():
-    print(boundary_change("123456789", 0))
-    print(boundary_change("123456789", 1))
-    print(boundary_change("123456789", 2))
-
-
 def test_schedule():
-    print(schedule(["0123456789"], 1000))
+    print(schedule(["0123456789"], [1], 1000))
 
 if __name__ == "__main__":
     test_schedule()
